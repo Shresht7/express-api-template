0576f1ba70de034d71cce5a50357d11080c83715
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-09-06 17:33:18 +0530

    Add useful middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 //  Library
 import express from 'express'
+import morgan from 'morgan'
+import helmet from 'helmet'
+import cors from 'cors'
 
 //  Routers
 import api from './api'
@@ -7,13 +10,23 @@ import api from './api'
 //  Middlewares
 import * as middleware from './middlewares'
 
+//  Type Definitions
+import { MessageResponse } from './types'
+
+//  dotenv
+import dotenv from 'dotenv'
+dotenv.config()
+
 //  -----------------
 const app = express()
 //  -----------------
 
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get<{}, MessageResponse>('/', (req, res) => {
     res.json({
         message: "Hello World!"
     })
