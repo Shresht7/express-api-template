50632022e090cfe8bd3dbfd59b0fac2c874ec68b
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2022-09-06 16:50:38 +0530

    Setup Express App

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
+//  Library
+import app from './app'
 
+//  -----------------------------------
+const PORT = process.env.PORT || '3000'
+//  -----------------------------------
+
+//  Start Server
+app.listen(PORT, () => {
+    console.log(`Server running: http://localhost:${PORT}`)
+})
